e1d8a2c5c776c79012b19ddb08f7b2b79cfc9088
Omega Mputu <kutekemeni93@gmail.com>
2022-01-27 19:00:16 +0100

    Barre de navigation créer

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname});
 });
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
